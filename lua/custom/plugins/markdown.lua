8304363673285e172b7b0fb8db91f172e82155fb
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2025-03-19 18:18:56 -0400

    prettier -> prettierd & del coderunner

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -6,9 +6,9 @@ return {
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
-  config = function ()
-require('render-markdown').setup({
-    anti_conceal = {
+  config = function()
+    require('render-markdown').setup {
+      anti_conceal = {
         enabled = true,
         -- Which elements to always show, ignoring anti conceal behavior. Values can either be booleans
         -- to fix the behavior or string lists representing modes where anti conceal behavior will be
@@ -16,12 +16,12 @@ require('render-markdown').setup({
         --  head_icon, head_background, head_border, code_language, code_background, code_border
         --  dash, bullet, check_icon, check_scope, quote, table_border, callout, link, sign
         ignore = {
-            code_background = true,
-            sign = true,
+          code_background = true,
+          sign = true,
         },
         above = 0,
         below = 0,
-    },
-})
-end
+      },
+    }
+  end,
 }
